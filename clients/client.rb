c782df0ee696db333f230171f3c90ed592585ab8
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-30 12:25:50 +0100

    Make server and client run until user quits

--- clients/client.rb
+++ clients/client.rb
@@ -1,8 +1,12 @@
 require 'socket'
 
 socket = TCPSocket.new('localhost', 2345)
-puts socket.gets
-puts "Wow what a rude server... I'm going to say Bob"
-socket.puts "Bob"
-puts socket.gets
+name = ""
+while name !='quit' do
+  puts socket.gets
+  puts "Wow what a rude server... Lets think about a name"
+  name = gets.chomp
+  socket.puts name
+  puts socket.gets
+end
 socket.close
--- clients/server.rb
+++ clients/server.rb
@@ -2,7 +2,13 @@ require 'socket'
 
 server = TCPServer.new(2345)
 socket = server.accept
-socket.puts "Hello client, what's your name?"
-name = socket.gets.chomp
-socket.puts "What a strange name #{name} is!"
+
+name = ""
+
+while name != 'quit' do
+  socket.puts "Hello client, what's your name?"
+  name = socket.gets.chomp
+  socket.puts "What a strange name #{name} is!"
+  socket.puts "Try again or quit"
+end
 socket.close
